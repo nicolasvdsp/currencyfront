0d7379bcd3004ffe024b88dc25a2eb46db4f6b4d
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2022-05-16 22:46:50 +0200

    feat(transactions): distinguish sender from the receiver

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -6,7 +6,7 @@
     const balance = 48;
     let myTransactions = ref([]);
 
-
+    const currentUser = 'Nicolas';
 
     onMounted(() => {
         fetch('http://localhost:3001/transactions')
@@ -32,9 +32,13 @@
   <div class="transactions">
       <h3 class="transactions__title">recentTransactions</h3>
       <div class="transactions__item" v-for="(t, index) in myTransactions" :key="index">
-            <p class="transactions__item__name">{{ t.sender }}</p>
+            <p v-if="t.sender === currentUser" class="transactions__item__name">{{ t.receiver }}</p>
+            <p v-else class="transactions__item__name">{{ t.sender }}</p>
+
             <span class="transactions__item__date">{{ moment(t.date).format('ll') }}</span>
-            <span class="transactions__item__amount transactions__item__amount--out">{{ t.amount }}<span> cC</span></span>
+            
+            <span v-if="t.sender === currentUser" class="transactions__item__amount transactions__item__amount--out">{{ t.amount }}<span> cC</span></span>
+            <span v-else class="transactions__item__amount transactions__item__amount--in">{{ t.amount }}<span> cC</span></span>
       </div>
   </div>
 </template> 
