1697fd900db1b5009f90347421e8ae23731bae44
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2022-05-19 03:31:41 +0200

    feat(transactions): show all users transactions in the transactions page

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -1,34 +1,41 @@
 <script setup>
     import { ref, onMounted } from 'vue'
-    const title = "All transactions";
+    import moment from 'moment';
+    import 'moment/dist/locale/en-ie';
+
+    moment.locale('en-ie');
+
+    const title = "allTransactions";
     let transactions = ref([1,2]);
+
+    const currentUser = 'Nicolas';
 
     onMounted(() => {
         fetch('http://localhost:3001/transactions')
             .then(res => res.json())
             .then(data => {
-                console.log(data);
                 transactions.value = data.data.transactions;
-                console.log(transactions);
             });
     });
 
 </script>
 
 <template>
-    <h1>{{ title }}</h1>
+    <h2>{{ title }}</h2>
 
     <div class="transactions">
-        <ul>
-            <li v-for="(t, index) in transactions" :key="index">{{ t.sender }} transfered {{ t.amount }} to {{ t.receiver }}</li>
-        </ul>
-    </div>
+      <div class="transactions__item" v-for="(t, index) in transactions" :key="index">
+            <p v-if="t.sender === currentUser" class="transactions__item__name">{{ t.receiver }}</p>
+            <p v-else class="transactions__item__name">{{ t.sender }}</p>
+
+            <span class="transactions__item__date">{{ moment(t.date).format('lll') }}</span>
+            
+            <span v-if="t.sender === currentUser" class="transactions__item__amount transactions__item__amount--out">{{ t.amount }}<span> cC</span></span>
+            <span v-else class="transactions__item__amount transactions__item__amount--in">{{ t.amount }}<span> cC</span></span>
+      </div>
+  </div>
 </template>
 
 <style scoped>
-ul {
-    max-width: 600px;
-    margin: 0 auto;
-    text-align: left;
-}
+
 </style>
