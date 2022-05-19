340663030fa547235abd8d0bf2554899cf3552f1
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2022-05-19 03:33:54 +0200

    feat(leaderboard): rank all users sorted on highest balance

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,11 +1,42 @@
 <script setup>
     import { ref, onMounted } from 'vue'
-    const title = "Leaderboard";
+
+    const title = "leaderBoard";
+    let allUsers = ref([1,2]);
+
+    onMounted(() => {
+        fetch('http://localhost:3001/users/getallbybalance')
+            .then(res => res.json())
+            .then(data => {
+                allUsers.value = data.data.users;
+            });
+    });
 
 </script>
 
 <template>
-    <h1>{{ title }}</h1>
+    <h2>{{ title }}</h2>
+
+    <div class="leaderboard">
+        <div class="leaderboard__item">
+            <p class="leaderboard__item__name">
+                <span class="leaderboard__item__ranking">#</span>
+                userName
+            </p>
+            <span class="leaderboard__item__balance">balance</span>
+        </div>
+        <hr style="margin-top: -0.5rem">
+
+        <div v-for="(u, index) in allUsers" :key="index" class="leaderboard__item">
+            <p class="leaderboard__item__name">
+                <span class="leaderboard__item__ranking">{{ index + 1 }}</span>
+                {{ u.username }}
+            </p>
+            <span class="leaderboard__item__balance">{{ u.balance }}</span>
+        </div>
+
+    </div>
+
 </template>
 
 <style scoped>
