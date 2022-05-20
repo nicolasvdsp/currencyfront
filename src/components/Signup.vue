a94baa4ab5f4cf2a0451cc96f0e255ffa3507f90
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-05-20 19:39:57 +0200

    feat: signup form now shows errors

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,4 +1,5 @@
 <template>
+<span class="error-message-login"></span>
     <form @submit.prevent="submit">
         <h1>Sign up</h1>
         <input v-model="data.username" placeholder="Username" required>
@@ -31,12 +32,17 @@ import {reactive} from 'vue';
                     body: JSON.stringify(data)
                 });
                 const json = await response.json();
-                console.log(json);
+              
                 //if json status is success then redirect to login page
                 if(json.status == "success") {
-                   window.location.href = '#/login';
+                   window.location.href = '#/transactions';
                 }
-
+                //else log errors
+                else {
+                    //get query selector
+                    const errorMessage = document.querySelector('.error-message-login');
+                    errorMessage.innerHTML = json.message;
+                }
              
                 
              
